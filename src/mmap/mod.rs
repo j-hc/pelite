c601570bab1bad6b68c59193370602df71a13ee1
j-hc <25510067+j-hc@users.noreply.github.com>
2022-07-21 21:54:34 +0300

    winapi -> windows-rs

--- src/mmap/mod.rs
+++ src/mmap/mod.rs
@@ -2,12 +2,12 @@
 Memory map files from disk.
 */
 
-#[cfg(all(windows, feature = "winapi"))]
+#[cfg(all(windows, feature = "windows-sys"))]
 mod windows;
-#[cfg(all(windows, feature = "winapi"))]
+#[cfg(all(windows, feature = "windows-sys"))]
 pub use self::windows::{FileMap, ImageMap};
 
 #[cfg(all(unix, feature = "libc"))]
 mod unix;
 #[cfg(all(unix, feature = "libc"))]
-pub use self::unix::{FileMap};
+pub use self::unix::FileMap;
